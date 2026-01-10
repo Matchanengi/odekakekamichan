cb48a0fb663def4a7547e8d3e420c32727fac04f
yokoyama <270392q@ugs.kochi-tech.ac.jp>
2026-01-10 23:35:32 +0900

    Supabaseクライアント追加

---
+++ src/components/supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  'https://sfzjvpordilroakqmjnp.supabase.co',
+  'sb_publishable_pgGTrddwsu5qrYd0Y7XLDw__jefOjKV'
+);
