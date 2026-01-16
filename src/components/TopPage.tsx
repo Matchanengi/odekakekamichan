3c3820c06a232b8aa18318e7fdcf323460b3729c
yokoyama <270392q@ugs.kochi-tech.ac.jp>
2026-01-17 06:19:54 +0900

    npm run buildできる形に修正

--- src/components/TopPage.tsx
+++ src/components/TopPage.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import { Sidebar } from '../components/Sidebar';
-import { ApprovalsPage } from './ApprovalsPage';
+//import { ApprovalsPage } from './ApprovalsPage';
 import { DashboardPage } from './DashboardPage';
 import { DeparturesPage } from './DeparturesPage';
 
-type Page = 'dashboard' | 'approvals' | 'departures';
+type Page = 'dashboard' | 'departures';
 
 export function TopPage() {
-  const [currentPage, setCurrentPage] = useState<Page>('approvals');
+  const [currentPage, setCurrentPage] = useState<Page>('dashboard');
 
   return (
     <div className="bg-green-700 rounded-3xl p-3 sm:p-8">
@@ -15,7 +15,8 @@ export function TopPage() {
         <Sidebar currentPage={currentPage} onPageChange={setCurrentPage} />
         <div className="flex-1 md:pl-8 mt-4 md:mt-0">
           {currentPage === 'dashboard' && <DashboardPage />}
-          {currentPage === 'approvals' && <ApprovalsPage />}
+          {/* 3. ApprovalsPage の表示条件を削除 */}
+          {/* {currentPage === 'approvals' && <ApprovalsPage />} */}
           {currentPage === 'departures' && <DeparturesPage />}
         </div>
       </div>
